5daa19ae6fb46e848564f437aadaf6aa276bc947
luizv <iam.luizv@gmail.com>
2021-05-03 11:54:46 -0300

    Adiciona lista 4 sobre loops

--- Exercicios.playground/Pages/Menu.xcplaygroundpage/Contents.swift
+++ Exercicios.playground/Pages/Menu.xcplaygroundpage/Contents.swift
@@ -3,5 +3,6 @@
  
  1. [Constantes e Variáveis](1%20Constantes%20e%20Variaveis)
  2. [Funções](2%20Funcoes)
- 3. [Condicionais](2%20Condicionais)
+ 3. [Condicionais](3%20Condicionais)
+ 4. [Loops](4%20Loops)
  */
