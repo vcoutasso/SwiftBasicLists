0e7890641da82db0cc21195ff2e8b4d8cda46d8a
luizv <iam.luizv@gmail.com>
2021-05-11 01:49:51 -0300

    adiciona lista 5 sobre vetores

---
+++ Exercicios.playground/Pages/5 Vetores.xcplaygroundpage/Contents.swift
@@ -0,0 +1,154 @@
+import UIKit
+/*:
+ # Lista 5: Vetores (Arrays / Listas)
+ ### 1. Contando as vogais [🐝]
+ Faça uma função que receba uma lista de caracteres e diga quantas vogais existem nessa lista. A função deve imprimir no console todas as vogais e retornar a quantidade.
+ */
+// código aqui
+
+/*:
+ ### 2. Conte os saguis [🐝]
+ Joana levou seu irmãozinho para um passeio na floresta para ver os saguis. Joana sabe que existe um certo número de saguis lá (n) mas seu irmãozinho é muito novo e, para apreciar o número inteiro, ele tem que começar a contar de 1.
+
+ Como uma boa irmã, Joana vai sentar e contar com ele. Dado um número (n), crie uma função que popule e retorne um array com todos os números até n e incluindo n, mas excluindo zero.
+ */
+ // código aqui
+
+/*:
+ ### 3. Primeiro item  [🐝]
+    Dada uma lista de inteiros, imprima o primeiro elemento.
+ */
+ // código aqui
+
+/*:
+ ### 4. Soma de listas [🐝]
+ Dadas duas listas de tamanho igual, retorne uma nova lista com a soma índice por índice das outras duas listas
+ */
+ // código aqui
+
+/*:
+ ### 5. Soma de Todos os Números [🐝]
+ Escreva um programa que soma todos os números de uma lista de inteiros.
+ */
+ // código aqui
+
+/*:
+ ### 6. Soma de Três Números [🐝]
+ Escreva um programa que soma os três primeiros números de uma lista de inteiros. Assuma que a lista sempre terá tamanho maior ou igual a três
+ */
+ // código aqui
+
+/*:
+ ### 7. Soma, multiplicação e média [🐝]
+ Faça uma função que receba uma lista de números inteiros, mostre a soma, a multiplicação e a média dos números.
+ */
+ // código aqui
+
+/*:
+ ### 8. Tem 0 no começo ou no final? [🐝]
+ Dada uma lista de inteiros, retorne se o primeiro ou o último elemento é 0. Assuma que a lista sempre vai ter tamanho > 0.
+ */
+ // código aqui
+
+/*:
+ ### 9. Último item [🐝]
+ Dada uma lista de inteiros, imprima o último elemento.
+ */
+ // código aqui
+
+/*:
+ ### 10. Acima da média 1 [🐝🐝]
+ Faça uma função que receba uma lista com as notas finais de um grupo de estudantes. A função deve imprimir a média de notas da classe e quantos alunos possuem nota final superior à média do grupo.
+ */
+ // código aqui
+
+/*:
+ ### 11. Mais Verdadeiros Ou Falsos? [🐝🐝]
+ Dada uma lista do tipo `Bool` retorne se ela tem mais elementos verdadeiros ou falsos.
+ Caso a lista esteja vazia ou o número de verdadeiros seja igual ao de falsos, retorne `true`
+ */
+ // código aqui
+
+/*:
+ ### 12. Palíndromo [🐝🐝]
+ Faça uma função que verifica se uma String é um palíndromo ou não
+ */
+ // código aqui
+
+/*:
+ ###  13. Separando pares e ímpares [🐝🐝]
+ Faça uma função que receba uma lista de números inteiros. A função deve armazenar os números pares dessa lista em uma segunda lista e os números ímpares em uma terceira lista. A função deve imprimir os valores das três listas.
+ */
+ // código aqui
+
+/*:
+ ### 14. Soma até X [🐝🐝]
+ Dadas uma lista e um valor inteiro limite, diga quantos números da lista são necessários somar para que se chegue no valor limite.
+
+ Caso a soma de todos os elementos da lista seja menor do que o valor limite, retorne nulo.
+ */
+ // código aqui
+
+/*:
+ ### 15. Substitua todos os números [🐝🐝]
+ Crie uma função chamada ReplaceAll que substitua todas as ocorrências de um número em uma lista, trocando-o por outro número. A função terá o seguinte formato:
+
+ ````
+ func replaceAll(array: [Int], antigo: Int, novo: Int) -> [Int] {
+ ...
+ }
+ ````
+
+ *Sugestão bônus para programadores avançados*
+
+ É possível deixar essa função genérica, para receber qualquer tipo de dado. Que tal  dar uma investigada em como transformar essa função em uma função genérica? Depois, pode olhar também como foi resolvido no CodeWars.
+ */
+ // código aqui
+
+/*:
+ ###  16. Tem menor? [🐝🐝]
+ Dado uma lista de números e um elemento mínimo, retorne se a lista possuí um elemento menor que o mínimo recebido.
+ */
+ // código aqui
+
+/*:
+ ### 17. Acima da média 2 [🐝🐝🐝]
+ Modifique o exercício `Acima da Média 1` para que a função receba também uma segunda lista com o nome dos alunos. Agora a função deve mostrar, ao invés da quantidade, o nome dos alunos que possuem nota final superior à média.
+ */
+ // código aqui
+
+/*:
+ ### 18. Soma da Matriz [🐝🐝🐝]
+ Faça uma função que receba uma matriz de inteiros e some todos os elementos dessa matriz (lista de listas)!
+ 
+ */
+ // código aqui
+
+/*:
+ ### 19. Linha dos Triângulos Ímpares [🐝🐝🐝🤯]
+ Dado um triângulo de números impares consecutivos, como por exemplo:
+ ````
+             1
+          3     5
+        7     9    11
+    13    15    17    19
+21    23    25    27    29
+ ...
+ ````
+ Faça uma função que calcule a soma dos números de uma linha n desse triângulo (a primeira linha começa com o index 1) e.g.:
+
+ ````
+ rowSumOddNumbers(1) // 1
+ rowSumOddNumbers(2) // 3 + 5 = 8
+ ````
+
+ ***Dica:*** pesquise sobre números triangulares.
+*/
+ // código aqui
+
+/*:
+ ### 20. Ordenação de Vetor [🐝🐝🐝🤯]
+ SEM USAR O  ```sort``` PADRÃO DO SWIFT, escreva uma função para ordenar um vetor de inteiros em ordem crescente.
+ */
+ // código aqui
+
